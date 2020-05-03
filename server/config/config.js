5d0cadde95eb4b04447ad2b11d5a1dd5edbae91e
unknown <desarrollador1982@gmail.com>
2020-05-03 10:53:34 +0200

    primer commit

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+
+
+
+//el process es un objeto global que esta corriendo a lo largo de toda la applicaicon node
+
+///=============
+// Puerto
+///=============
+process.env.PORT = process.env.PORT || 3000;
